771d739f22e287e6fc36e802a30de96c802eba84
Abdul Waleed <abdulwaleedfsd@gmail.com>
2025-05-17 10:24:01 -0400

    Build CarModel and CarMake Django Models

--- server/djangoapp/models.py
+++ server/djangoapp/models.py
@@ -23,3 +23,43 @@
 # - Year (IntegerField) with min value 2015 and max value 2023
 # - Any other fields you would like to include in car model
 # - __str__ method to print a car make object
+
+
+# -----------------------------------------------------------------
+
+from django.db import models
+from django.utils.timezone import now
+from django.core.validators import MaxValueValidator, MinValueValidator
+
+class CarMake(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.TextField()
+
+    def __str__(self):
+        return self.name
+
+
+class CarModel(models.Model):
+    car_make = models.ForeignKey(CarMake, on_delete=models.CASCADE)  # One-to-Many
+    name = models.CharField(max_length=100)
+
+    CAR_TYPES = [
+        ('SEDAN', 'Sedan'),
+        ('SUV', 'SUV'),
+        ('WAGON', 'Wagon'),
+        # Add more if needed
+    ]
+    type = models.CharField(max_length=10, choices=CAR_TYPES, default='SUV')
+    year = models.IntegerField(
+        default=2023,
+        validators=[
+            MaxValueValidator(2023),
+            MinValueValidator(2015)
+        ]
+    )
+    dealer_id = models.IntegerField()  # Link to external dealer data (e.g. from Cloudant)
+
+    def __str__(self):
+        return f"{self.car_make.name} {self.name} ({self.year})"
+
+
